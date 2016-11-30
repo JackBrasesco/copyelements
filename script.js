ba9e3012447224c2fb6b338edf2dde71e7863ab8
JackBrasesco <jacbras@nuevaschool.org>
2016-11-30 08:42:53 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -6,8 +6,15 @@
 // 5. how do you repeat this multiple times, for multiple colors??? There is a nice data structure for this kind of thing.
 
 var elementsList = $("#elements-list")
-
-$(".element").clone().appendTo(elementsList)
-$(".element").clone().appendTo(elementsList)
-$(".element").clone().appendTo(elementsList)
-$(".element").clone().appendTo(elementsList)
+//
+//$(".element").clone().appendTo(elementsList)
+//$(".element").clone().appendTo(elementsList)
+//$(".element").clone().appendTo(elementsList)
+//$(".element").clone().appendTo(elementsList)
+//$(".element").clone().appendTo(elementsList)
+//$(".element").clone().appendTo(elementsList)
+//$(".element").clone().appendTo(elementsList)
+//$(".element").clone().appendTo(elementsList)
+//$(".element").clone().appendTo(elementsList)
+//$(".element").clone().appendTo(elementsList)
+//$(".element").clone().appendTo(elementsList)
